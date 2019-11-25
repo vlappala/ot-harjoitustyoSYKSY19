598b44e7072839d1492f686681ada378af78b700
Ville Lappalainen <ville.lappalainen@helsinki.fi>
2019-11-25 14:45:35 +0200

    saatoa, maven ei tykkaa guista

--- ot-Arithmetix/src/main/java/com/mycompany/ot/arithmetix/Main.java
+++ ot-Arithmetix/src/main/java/com/mycompany/ot/arithmetix/Main.java
@@ -18,7 +18,7 @@ public class Main {
     
     public static void main(String[] args) {
         
-        Ui.main(args);
+        // Ui.main(args);
         
         Engine game = new Engine();
         Scanner reader = new Scanner(System.in);
